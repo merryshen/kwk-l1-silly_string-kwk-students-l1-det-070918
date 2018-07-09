d8e092a690bbf1f8df03532ba60974b82b84fc04
merryshen <yihuaca@gmail.com>
2018-07-09 20:16:56 +0000

    Done.

--- silly_string.rb
+++ silly_string.rb
@@ -1,4 +1,10 @@
-
+# puts "Fabulous"
+# puts "Fabulous".upcase
+# puts "IM NOT SHOUTING".downcase
+# puts "supercalifragilisticexpialidocious".length
+# puts "wow"+"mom"
+# puts "i really like programming".capitalize
+# puts "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!".gsub("A","Z").gsub("a","o")
 # Challenge 1: Fill this string in with your favorite word
 
 your_favorite_word = ""
